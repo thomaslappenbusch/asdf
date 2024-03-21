b1f0548e1b57235c82b3c0b3a46b6086f3916e4d
Thomas Lappenbusch <thomaslappenbusch@gmail.com>
2024-03-21 15:45:18 -0700

    1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,8 @@
+<svelte:head>
+   <title>The Vauren Institute</title>
+</svelte:head>
+
+
 <script>import "../app.pcss";</script>
 <div class="navbar bg-base-200 shadow-sm fixed z-10"> <!-- bg-base-100 shadow-md -->
     <div class="navbar-center flex-1 max-w-2xl mx-auto justify-center">
